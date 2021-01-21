49d39d3e2c4ed350a523ba3fe11e89ee8e37443d
JC-Garads <74990063+JC-Garads@users.noreply.github.com>
2021-01-21 16:45:48 +0800

    my answers my answer on challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const time = new Date().getHours();
+var greetings = "";
+const customStyle = {
+  color: ""
+};
+if (time >= 0 && time < 12) {
+  greetings = "morning";
+  customStyle.color = "red";
+} else if (time >= 12 && time < 18) {
+  greetings = "afternoon";
+  customStyle.color = "green";
+} else {
+  greetings = "evening";
+  customStyle.color = "blue";
+}
+
+ReactDOM.render(
+  <h1 style={customStyle} className="heading">
+    Good {greetings}
+  </h1>,
+  document.getElementById("root")
+);
+
 //Create a React app from scratch.
 //Show a single h1 that says "Good morning" if between midnight and 12PM.
 //or "Good Afternoon" if between 12PM and 6PM.
